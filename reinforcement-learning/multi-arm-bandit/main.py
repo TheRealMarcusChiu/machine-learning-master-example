08aa9a05d4349fb32be561b0c7b1ce35e80fc114
Marcus Chiu <marcuschiu9@gmail.com>
2018-12-25 00:58:54 -0600

    updated reinforement-learning Multi-Arm Bandit Program

--- reinforcement-learning/multi-arm-bandit/main.py
+++ reinforcement-learning/multi-arm-bandit/main.py
@@ -25,14 +25,15 @@ class Type(enum.Enum):
 class MultiArmBanditLearner:
 
     def __init__(self, q_estimate: [float], epsilon: float, t: Type, constant_step_size=None):
-        self.q_estimate = q_estimate
         self.epsilon = epsilon
         self.reward_total = 0.0
 
         self.type = t
         if t == Type.SAMPLE_AVERAGE:
+            self.q_estimate = [0] * len(q_estimate)
             self.n = [0] * len(self.q_estimate)
         elif t == Type.CONSTANT_STEP_SIZE:
+            self.q_estimate = q_estimate
             self.constant_step_size = constant_step_size
 
     def get_step_size(self, index: int):
@@ -64,31 +65,44 @@ class EnvironmentType(enum.Enum):
 
 if __name__ == "__main__":
 
+    mean = 0
+    std = 1
+    size = 10
+    environment = MultiArmBandit(np.random.normal(mean, std, size))
+    print(environment.q)
+
+    eps = 0.1
+    ss = 0.1
+
+
     def run(t: EnvironmentType):
-        q_real = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
-        environment = MultiArmBandit(q_real)
 
-        eps = 0.1
-        ss = 0.1
-        initial_q_estimate = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        rl_css_0 = MultiArmBanditLearner([0] * size, eps, Type.CONSTANT_STEP_SIZE, ss)
 
-        rl_css = MultiArmBanditLearner(initial_q_estimate.copy(), eps, Type.CONSTANT_STEP_SIZE, ss)
-        rl_sa  = MultiArmBanditLearner(initial_q_estimate.copy(), eps, Type.SAMPLE_AVERAGE, None)
+        # initial q estimate values = 5, therefore encourages exploration, bc true q values are mean=0 and var=std=1
+        # The result is that all actions are tried several times before the value estimates converge.
+        # The system does a fair amount of exploration even if greedy actions are selected all the time
+        # look at (Constant Step Size - Initial Q Estimates Graph.png)
+        rl_css_5 = MultiArmBanditLearner([5] * size, eps, Type.CONSTANT_STEP_SIZE, ss)
+        rl_sa  = MultiArmBanditLearner([0] * size, eps, Type.SAMPLE_AVERAGE, None)
 
         for i in range(10000):
-            rl_css.step(environment)
+            rl_css_0.step(environment)
+            rl_css_5.step(environment)
             rl_sa.step(environment)
 
             if t == EnvironmentType.DYNAMIC:
                 environment.update_q_star()
 
-        print("rl constant step size - reward total: " + str(rl_css.reward_total))
-        print("rl sample average     - reward total: " + str(rl_sa.reward_total))
+        print("constant step size (initial q estimates = 0) reward total: " + str(rl_css_0.reward_total))
+        print("constant step size (initial q estimates = 5) reward total: " + str(rl_css_5.reward_total))
+        print("sample average                               reward total: " + str(rl_sa.reward_total))
+
 
     # Static Environment
-    print("Environment Values Static - RL Sample Average should have higher reward total in theory")
+    print("Static Multi-Armed Bandit - Sample Average Learner in theory should have higher reward total")
     run(EnvironmentType.STATIC)
 
     # Dynamic Environment
-    print("\nEnvironment Values Dynamic - RL Constant Step Size should have higher reward total in theory")
+    print("\nDynamic Multi-Armed Bandit - Constant Step Size Learner in theory should have higher reward total")
     run(EnvironmentType.DYNAMIC)
